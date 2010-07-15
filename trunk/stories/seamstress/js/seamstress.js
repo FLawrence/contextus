6492d33348b87d3de6fd2ecc340f903f557fddb2
mjewell <mjewell@91e44848-8282-11de-92f0-6bbadbc12360>
2010-07-15 12:43:49 +0000

    JS tweaks

--- trunk/stories/seamstress/js/seamstress.js
+++ trunk/stories/seamstress/js/seamstress.js
@@ -3,6 +3,7 @@ var sparqler = new SPARQL.Service("http://contextus.net/sparql/");
 
 function findObjects(uri, keywords)
 {
+    alert("URI: "+uri+" keywords: "+keywords);
     var query = sparqler.createQuery();
     query.query("SELECT * WHERE {?s ?p ?o} LIMIT 10", {
         failure: function(xhr) { alert(xhr.statusText); },
