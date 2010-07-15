e8eef611099cadeb87dacfb9e959b6722258c4b8
mjewell <mjewell@91e44848-8282-11de-92f0-6bbadbc12360>
2010-07-15 12:27:05 +0000

    Initial work on Seamstress

---
+++ stories/seamstress/js/seamstress.js
@@ -0,0 +1,11 @@
+var sparqler = new SPARQL.Service("http://contextus.net:7000/sparql/");
+
+
+function findObjects(uri, keywords)
+{
+    var query = sparqler.createQuery();
+    query.query("SELECT * WHERE {?s ?p ?o} LIMIT 10", {
+        failure: function() { alert("boo"); },
+        success: function(json) { alert("woo"); }
+    });    
+}
