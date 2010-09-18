9f5a83d17c7466ba0b865df62094b5b3a41ec677
KludgeKML <KludgeKML@91e44848-8282-11de-92f0-6bbadbc12360>
2010-09-18 12:14:14 +0000

    Added the first shot at the Midsummer Night's Dream character editor.

---
+++ tei/shakespeare/Editor/index.php
@@ -0,0 +1,47 @@
+<?php
+require 'lightopenid/openid.php';
+
+try
+{
+	if(!isset($_GET['openid_mode']))
+	{
+		if(isset($_POST['openid_identifier']) || isset($_POST['Google']))
+		{
+			$openid = new LightOpenID;
+
+			if (isset($_POST['Google']))
+	            $openid->identity = 'https://www.google.com/accounts/o8/id';
+			else
+				$openid->identity = $_POST['openid_identifier'];
+
+			header('Location: ' . $openid->authUrl());
+		}
+?>
+<form action="" method="post">
+    OpenID: <input type="text" name="openid_identifier" /> <button>Login via OpenId</button>
+</form>
+<form action="" method="post">
+	<input type="hidden" name="Google" value="Google" />
+    Google: <button>Login via Google</button>
+</form>
+
+<?php
+
+	}
+	elseif($_GET['openid_mode'] == 'cancel')
+	{
+		echo 'User has canceled authentication!';
+	}
+	else
+	{
+		$openid = new LightOpenID;
+		if ($openid->validate())
+			header('Location: characteredit.php?idhash=' . sha1($openid->authUrl()));
+		else
+			echo 'User ' . $openid->identity . ' has not logged in.';
+	}
+}
+catch(ErrorException $e)
+{
+	echo $e->getMessage();
+}
---
+++ tei/shakespeare/Editor/savedata.php
@@ -0,0 +1,45 @@
+<?php
+
+require 'fourstore-php/Store.php';
+require 'fourstore-php/Namespace.php';
+
+require '/usr/share/php/libzend-framework-php/Zend/Loader/Autoloader.php';
+spl_autoload_register(array('Zend_Loader_Autoloader', 'autoload'));
+
+//print_r($_POST);
+//print("<br /><br />");
+
+$changes = explode("|", $_POST['alteredData']);
+
+$graphUser = 'http://contextus.net/resource/midsum_night_dream/' . $_POST['idhash'] .  '/';
+
+$s = new FourStore_Store('http://contextus.net:7000/sparql/');
+
+foreach ($changes as $change)
+{
+	$parts = split("=", $change);
+
+	if (count($parts) != 2) continue;
+
+	$subject = $graphUser . 'character/' . $parts[0];
+	$object = $parts[1];
+
+	//print('Asking: &lt;' . $subject . '&gt; &lt;http://xmlns.com/foaf/0.1/name&gt; ?o <br />');
+	$r = $s->ask('ASK WHERE {<' . $subject . '> <http://xmlns.com/foaf/0.1/name> ?o }');
+
+	if ($r == true)
+	{
+		//print('Setting: &lt;' . $subject . '&gt; &lt;http://xmlns.com/foaf/0.1/name&gt; "' . $object . '" . <br />');
+		$s->set($graphUser, '<' . $subject . '> a <http://purl.org/ontomedia/ext/common/being#Character> .');
+		$s->set($graphUser, '<' . $subject . '> <http://xmlns.com/foaf/0.1/name> "' . $object . '" .');
+	}
+	else
+	{
+		//print('Adding: &lt;' . $subject . '&gt; &lt;http://xmlns.com/foaf/0.1/name&gt; "' . $object . '" . <br />');
+		$s->add($graphUser, '<' . $subject . '> a <http://purl.org/ontomedia/ext/common/being#Character> .');
+		$s->add($graphUser, '<' . $subject . '> <http://xmlns.com/foaf/0.1/name> "' . $object . '" .');
+	}
+}
+
+header('Location: characteredit.php?idhash=' . $_POST['idhash']);
+?>
