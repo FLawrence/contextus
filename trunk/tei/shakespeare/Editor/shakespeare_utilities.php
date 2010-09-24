c4b02a863da31d780a286a7610fa0588ee03265c
KludgeKML <KludgeKML@91e44848-8282-11de-92f0-6bbadbc12360>
2010-09-24 15:26:47 +0000

    Add logout button

--- trunk/tei/shakespeare/Editor/shakespeare_utilities.php
+++ trunk/tei/shakespeare/Editor/shakespeare_utilities.php
@@ -22,6 +22,7 @@ function printNavigationList ( $current, $userID )
 		else
 			print('   <li><a href="' . $url .  '?idhash=' . $userID . '">' . $label . '</a></li>' . "\n");
 	}
+	print('   <li><a href="index.php">Logout</a></li>
 	print('</ul>' . "\n");
 }
 
