a116580af751e71baae5909284cd557df1a7cc83
KludgeKML <KludgeKML@91e44848-8282-11de-92f0-6bbadbc12360>
2010-10-07 16:12:48 +0000

    First draft of new characteredit.php system.

--- tei/shakespeare/Editor/functions.js
+++ tei/shakespeare/Editor/functions.js
@@ -18,6 +18,8 @@ function setupChooser ( )
 	updateFields();
 }
 
+
+
 function updateFields ( )
 {
 	item = store.findTriple(document.editForm.namedEntityList.options[document.editForm.namedEntityList.selectedIndex].value, nameLabel);
@@ -38,6 +40,8 @@ function updateFields ( )
 	createPropertyTable(store, item.getS());
 	checkFields();
 }
+
+
 
 function updateName ( )
 {
@@ -60,6 +64,8 @@ function updateName ( )
 	checkFields();
 }
 
+
+
 function checkFields ( )
 {
 	var dataString = "";
@@ -70,7 +76,7 @@ function checkFields ( )
 	{
 		foundTriple = originalStore.findTriple(triples[i].getS(), triples[i].getP());
 
-		if (triples[i].getO() != foundTriple.getO())
+		if ((foundTriple == null) || (triples[i].getO() != foundTriple.getO()))
 		{
 			dataString += triples[i].getS() + " " + triples[i].getP() + " " + triples[i].getO() + "\n";
 		}
@@ -89,30 +95,98 @@ function checkFields ( )
 }
 
 
+
 function createPropertyTable ( store, subject )
 {
 	var triples = store.getTriples();
 
-	var table = '<table><tr><th>Property</th><th>Value</th><th></th></tr>'
-
+	var table = '<form onsubmit="return false;" name="propertyTableForm"><table><tr><th>Property</th><th>Value</th><th></th></tr>'
 	for (i = 0; i < triples.length; i++)
 	{
 		if (triples[i].getS() == subject)
 		{
 			foundTriple = originalStore.findTriple(triples[i].getS(), triples[i].getP());
 
-			if (triples[i].getO() != foundTriple.getO())
+			for (j = 0; j < properties.length; j++)
 			{
-				table += '<tr><td>' + triples[i].getP() + '</td><td clas="changed">' + triples[i].getO() + '</td><td><a href="">[X]</a></td></tr>';
-			}
-			else
-			{
-				table += '<tr><td>' + triples[i].getP() + '</td><td>' + triples[i].getO() + '</td><td><a href="">[X]</a></td></tr>';
-			}
+				if ((properties[j].module + properties[j].property) == triples[i].getP())
+				{
+					var classAttribute = '';
+					var edit = triples[i].getO();
+
+					if ((foundTriple == null) || (triples[i].getO() != foundTriple.getO()))
+					{
+						classAttribute = ' class="changed"';
+					}
+
+					if (properties[j].subject == '*')
+					{
+						edit = '<input name="editProperty' + i + '" value="' + edit + '" onkeyup="updateName();"/>';
+					}
+	
+					table += '<tr><td>' + triples[i].getP() + '</td><td' + classAttribute + '>' + edit + '</td><td><button onclick="removeProperty(' + i + '); return false;">delete</button></td></tr>';
+				}
+			}	
+
 		}	
 	}
 
-	table += '</table>'
+	table += '<tr><td><select name="propertyList"><option value="http://purl.org/ontomedia/core/expression#is" name="http://purl.org/ontomedia/core/expression#is"/></select></td><td><select name="entityList"><option value="" name="please wait..."/></select></td><td><button onclick="addProperty();">add</button></td></tr>';
+
+	table += '</table></form>'
 
 	document.getElementById('propertyTable').innerHTML = table;
+
+	document.propertyTableForm.propertyList.options.length = 0;
+	var property = new Option('http://purl.org/ontomedia/core/expression#is', 'http://purl.org/ontomedia/core/expression#is', false, false);
+	document.propertyTableForm.propertyList.options[0] = property;
+
+	document.propertyTableForm.entityList.options.length = 0;
+	var index = 0;
+	for (i = 0; i < triples.length; i++)
+	{
+		if (triples[i].getS() == document.editForm.namedEntityList.options[document.editForm.namedEntityList.selectedIndex].value)
+		{
+			continue;
+		}
+
+		if (triples[i].getP() == nameLabel)
+		{
+			var option = new Option(triples[i].getO(), triples[i].getS(), false, false);
+			document.propertyTableForm.entityList.options[index] = option;
+			index++;
+		}
+	}
+}
+
+
+
+function addProperty ( )
+{
+	var triple = new Triple(document.editForm.namedEntityList.options[document.editForm.namedEntityList.selectedIndex].value,
+				document.propertyTableForm.propertyList.options[document.propertyTableForm.propertyList.selectedIndex].value,
+				document.propertyTableForm.entityList.options[document.propertyTableForm.entityList.selectedIndex].value);
+	store.add(triple);
+	createPropertyTable(store, triple.getS());
+	checkFields();
+}
+
+
+function removeProperty ( propertyIndex )
+{
+	store.deleteByIndex(propertyIndex);
+	updateFields();
+}
+
+
+function alertStore ( store )
+{
+	triples = store.getTriples();
+	var index = 0;
+	for (i = 0; i < triples.length; i++)
+	{
+		alert(	"S=" + triples[i].getS() + "\n" +
+			"P=" + triples[i].getP() + "\n" +
+			"O=" + triples[i].getO())
+	}
 }
